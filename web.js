47502c23c5264914750b348009a5586bebcdd20f
John Smith <example@stanford.edu>
2013-08-01 13:09:15 +0000

    web.js atualizado

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var file_buf = fs.readFileSync("index.html");
+  var text_file = file_buf.toString();
+  response.send(text_file);
+  /*response.send('Hello World 2!');*/
 });
 
 var port = process.env.PORT || 5000;
